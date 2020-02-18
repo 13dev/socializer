58cf777d91bf3fbe001e123a6ddff71885e962dd
13dev <qwerty124563@gmail.com>
2020-02-18 17:29:08 +0000

    Apply fixes from StyleCI

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class     => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class             => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class               => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
